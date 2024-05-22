4e7145cc4601c1da8aad969dd8e6399a76183368
ianindratama <ian.indratama@gmail.com>
2024-05-22 08:42:02 +0700

    Initial commit

---
+++ finish/app/src/main/java/com/ianindratama/myforegroundservices/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.ianindratama.myforegroundservices
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ianindratama.myforegroundservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean? ->
+        if (!isGranted!!)
+            Toast.makeText(this,
+                "Unable to display Foreground service notification due to permission decline",
+                Toast.LENGTH_LONG)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        binding.tvOsVersion.text = "Perangkat Android dengan API ${Build.VERSION.SDK_INT}"
+
+        val foregroundServiceIntent = Intent(this, TimerForegroundService::class.java)
+        binding.btnStart.setOnClickListener {
+            startForegroundService(foregroundServiceIntent)
+        }
+
+        binding.btnStop.setOnClickListener {
+            stopService(foregroundServiceIntent)
+        }
+    }
+}
---
+++ starter/app/src/main/java/com/ianindratama/myforegroundservices/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.ianindratama.myforegroundservices
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ianindratama.myforegroundservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.tvOsVersion.text = "Perangkat Android dengan API ${Build.VERSION.SDK_INT}"
+
+        val foregroundServiceIntent = Intent(this, TimerForegroundService::class.java)
+        binding.btnStart.setOnClickListener {
+            startForegroundService(foregroundServiceIntent)
+        }
+
+        binding.btnStop.setOnClickListener {
+            stopService(foregroundServiceIntent)
+        }
+    }
+}
